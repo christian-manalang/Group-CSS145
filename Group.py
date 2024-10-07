49ed2b5c4d870df458e4cc7ef9ee17cbbb5624cd
liandrorefulle <lerefulle@mymail.mapua.edu.ph>
2024-10-07 22:14:19 +0800

    my commit!!

--- Group.py
+++ Group.py
@@ -70,3 +70,25 @@ plt.axis('equal')
 plt.title('Distribution of GPU Companies', fontsize=16, fontweight='bold')
 
 plt.show()
+
+#Liandro1
+
+plt.figure(figsize=(10, 6))
+df['Company'].value_counts().plot(kind='bar', color='skyblue')
+plt.title('Count of Laptops by Company')
+plt.xlabel('Company')
+plt.ylabel('NUmber of Laptops')
+plt.xticks(rotation=45)
+plt.show()
+
+#Liandro2
+
+df = pd.read_csv('/laptop_price.csv')
+
+plt.figure(figsize=(10, 6))
+df['RAM (GB)'].value_counts().sort_index().plot(kind='barh', color='lightgreen')
+plt.title('Count of Laptops by RAM Size')
+plt.xlabel('Number of Laptops')
+plt.ylabel('RAM (GB)')
+plt.show()
+
