5af495fcfeb1c89fca4ad39fbfd84a7a3b9cd5f1
liandrorefulle <lerefulle@mymail.mapua.edu.ph>
2024-10-07 22:30:14 +0800

    my commit 2

--- Group.py
+++ Group.py
@@ -123,30 +123,6 @@ plt.show()
 
 #Liandro2
 
-df = pd.read_csv('/laptop_price.csv')
-
-plt.figure(figsize=(10, 6))
-df['RAM (GB)'].value_counts().sort_index().plot(kind='barh', color='lightgreen')
-plt.title('Count of Laptops by RAM Size')
-plt.xlabel('Number of Laptops')
-plt.ylabel('RAM (GB)')
-plt.show()
-
-
-#Liandro1
-
-plt.figure(figsize=(10, 6))
-df['Company'].value_counts().plot(kind='bar', color='skyblue')
-plt.title('Count of Laptops by Company')
-plt.xlabel('Company')
-plt.ylabel('NUmber of Laptops')
-plt.xticks(rotation=45)
-plt.show()
-
-#Liandro2
-
-df = pd.read_csv('/laptop_price.csv')
-
 plt.figure(figsize=(10, 6))
 df['RAM (GB)'].value_counts().sort_index().plot(kind='barh', color='lightgreen')
 plt.title('Count of Laptops by RAM Size')
